71753f05079b78689931c4a95cf76aac7b38ad30
储天航 <1193983801@qq.com>
2023-03-10 17:26:57 +0800

    🎉 init(配置): tailwind

---
+++ client_server/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
